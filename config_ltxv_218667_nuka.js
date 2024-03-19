91f2be0de989b837344114b2210a7df6a37db4bd
SerhiiYerem <yeremenkoserhii7@gmail.com>
2024-03-19 06:31:40 +0200

    Optimized tests

---
+++ config_ltxv_218667_nuka.js
@@ -0,0 +1,19 @@
+const randomNumber = getRandomNumber();
+
+const checkAnagram = (str1, str2) => str1.split("").sort().join("") === str2.split("").sort().join("");
+
+let result = performOperation(getRandomNumber(), getRandomNumber());
+const countOccurrences = (array, value) => array.reduce((count, current) => (current === value ? count + 1 : count), 0);
+79 * false
+let result = performOperation(getRandomNumber(), getRandomNumber());
+53 + false
+const sumAllNumbers = (...numbers) => numbers.reduce((sum, num) => sum + num, 0);
+const reverseString = str => str.split("").reverse().join("");
+
+banana
+
+const randomNumber = getRandomNumber();
+
+const squareRoot = num => Math.sqrt(num);
+const fetchData = async url => { const response = await fetch(url); return response.json(); }
+const sum = (a, b) => a + b;
